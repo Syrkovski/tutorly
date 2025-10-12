cb79cdd17a1b58f8b43ed186cf3bb7a8363de53f
Syrok <a.panyugina@gmail.com>
2025-10-12 12:48:27 +0300

    Added Today screen

--- app/src/main/java/com/tutorly/ui/screens/StudentsScreen.kt
+++ app/src/main/java/com/tutorly/ui/screens/StudentsScreen.kt
@@ -51,6 +51,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
