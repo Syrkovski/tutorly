368951b72c7f84fb02e4a05963484f9d5b1b77ed
Syrok <a.panyugina@gmail.com>
2025-10-27 16:04:01 +0300

    Fix bracket

--- app/src/main/java/com/tutorly/ui/screens/StudentEditorScreen.kt
+++ app/src/main/java/com/tutorly/ui/screens/StudentEditorScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
