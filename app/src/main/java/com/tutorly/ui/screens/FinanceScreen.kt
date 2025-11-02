e5074ad26bf8b1be309c2014826caf59be1ab2ea
Syrkovski <a.panyugina@gmail.com>
2025-11-02 15:07:47 +0300

    Fix finance tab indicator import

--- app/src/main/java/com/tutorly/ui/screens/FinanceScreen.kt
+++ app/src/main/java/com/tutorly/ui/screens/FinanceScreen.kt
@@ -33,7 +33,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
-import androidx.compose.material3.tabIndicatorOffset
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
