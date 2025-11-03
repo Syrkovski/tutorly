00828c732b2362b9f20504385e97fa05e7fe5ab8
Syrok <a.panyugina@gmail.com>
2025-11-03 13:03:50 +0300

    Fix bracket

--- app/src/main/java/com/tutorly/ui/screens/StudentDetailsScreen.kt
+++ app/src/main/java/com/tutorly/ui/screens/StudentDetailsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
